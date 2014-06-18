4fd32b3e236cc92b1e130dff74a4cc07302f6064
apple <apple@applematoiMac.local>
2014-06-18 13:10:05 +0800

    Initial Commit

---
+++ Tourism/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tourism
+//
+//  Created by apple on 14-6-18.
+//  Copyright (c) 2014年 com.youyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tourism/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tourism
+//
+//  Created by apple on 14-6-18.
+//  Copyright (c) 2014年 com.youyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
